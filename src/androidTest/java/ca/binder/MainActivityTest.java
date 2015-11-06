39bcb277e55a31f2492b45af95c63790cd65958d
roddicks <sheldon.roddick@hotmail.com>
2015-11-05 19:12:06 -0800

    gg kotlin

---
+++ src/androidTest/java/ca/binder/MainActivityTest.java
@@ -0,0 +1,15 @@
+package ca.binder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by SheldonCOMP4980 on 11/5/2015.
+ */
+public class MainActivityTest {
+
+    @Test
+    public void testsShouldWork() {
+        Assert.assertTrue("Tests should run on-device", true);
+    }
+}
---
+++ src/main/java/ca/binder/MainActivity.java
@@ -0,0 +1,15 @@
+package ca.binder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by SheldonCOMP4980 on 11/5/2015.
+ */
+public class MainActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
