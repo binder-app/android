3e99568252cba979f8d9cc81d787ac8eb8406dd3
Steve <stevenlyall@gmail.com>
2015-11-12 22:35:18 -0800

    removed support library

--- src/main/java/ca/binder/android/NullPhoto.java
+++ src/main/java/ca/binder/android/NullPhoto.java
@@ -2,7 +2,6 @@ package ca.binder.android;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
 
 import ca.binder.R;
 import ca.binder.domain.IPhoto;
@@ -14,6 +13,6 @@ import ca.binder.domain.IPhoto;
 public class NullPhoto implements IPhoto {
     @Override
     public Drawable getDrawable(Context context) {
-        return ContextCompat.getDrawable(context, R.drawable.default_photo);
+        return context.getDrawable(R.drawable.default_photo);
     }
 }
