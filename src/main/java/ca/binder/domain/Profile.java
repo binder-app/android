b8c0227b77fb218eab968e00e2e123e5e3fe67cd
Mitchell Hentges <mitch9654@gmail.com>
2015-11-19 18:54:52 -0800

    Set up Profile persistence

--- src/main/java/ca/binder/domain/Profile.java
+++ src/main/java/ca/binder/domain/Profile.java
@@ -1,9 +1,15 @@
 package ca.binder.domain;
 
-import ca.binder.remote.Photo;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.ArraySet;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import ca.binder.remote.Photo;
 
 /**
  * @author Mitchell Hentges
@@ -17,7 +23,7 @@ public class Profile {
     private String phone;
     private String year;
     private Photo photo;
-    private List<Course> courses = new ArrayList<>();
+    private Set<Course> courses = new HashSet<>();
 
     public Profile(String id) {
         this.id = id;
@@ -75,7 +81,7 @@ public class Profile {
         this.photo = photo;
     }
 
-    public List<Course> getCourses() {
+    public Set<Course> getCourses() {
         return courses;
     }
 
@@ -85,5 +91,54 @@ public class Profile {
 
     public void removeCourse(Course course) {
         courses.remove(course);
+    }
+
+    public void save(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        Set<String> courseSet = new HashSet<>();
+        for (Course course : courses) {
+            courseSet.add(course.getName());
+        }
+
+        preferences.edit()
+                .putString("profile_id", id)
+                .putString("profile_bio", bio)
+                .putString("profile_phone", phone)
+                .putString("profile_program", program)
+                .putString("profile_year", year)
+                .putString("profile_name", name)
+                .putString("profile_photo", photo.base64())
+                .putStringSet("profile_courses", courseSet)
+                .apply();
+
+    }
+
+    /**
+     * Returns a profile from persistence, or null if it hasn't been saved before
+     * @param context Android context
+     * @return Profile from stored data, or null if no data exists
+     */
+    public static Profile load(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (!preferences.contains("profile_id")) {
+            return null;
+        }
+
+        ProfileBuilder builder = ProfileBuilder.start(preferences.getString("profile_id", ""))
+                .bio(fromPref(preferences, "profile_bio"))
+                .phone(fromPref(preferences, "profile_phone"))
+                .year(fromPref(preferences, "profile_year"))
+                .name(fromPref(preferences, "profile_name"))
+                .program(fromPref(preferences, "profile_program"))
+                .photo(new Photo(fromPref(preferences, "profile_photo")));
+        for (String courseName : preferences.getStringSet("profile_courses", new HashSet<String>())) {
+            builder = builder.course(new Course(courseName));
+        }
+
+        return builder.build();
+    }
+
+    private static String fromPref(SharedPreferences preferences, String key) {
+        return preferences.getString(key, "");
     }
 }
